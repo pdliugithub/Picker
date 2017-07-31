7147d6c10d10909139af8b37d465c3a5582aff3a
pd_liu <xkrossia>
2017-07-31 19:01:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pd_liu/picker/PickerActivity.java
@@ -0,0 +1,46 @@
+package com.example.pd_liu.picker;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class PickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picker);
+    }
+
+    public void datePicker(View view) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    Toast.makeText(PickerActivity.this, "i == " + i + ",,i1 == " + i1 + ",,i2" + i2, Toast.LENGTH_SHORT).show();
+                }
+            }, 2017, 7, 3);
+            dialog.show();
+        }
+    }
+
+    public void timePicker(View view) {
+        TimePickerDialog dialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                Toast.makeText(PickerActivity.this, "i == " + i + ",,i1 == " + i1, Toast.LENGTH_SHORT).show();
+            }
+        }, 1, 5, false);
+        dialog.show();
+    }
+
+    public void elseInform(View view){
+        startActivity(new Intent(this, FullscreenActivity.class));
+    }
+}
